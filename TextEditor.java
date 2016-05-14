fd7ea3b63c0abf5f1c43add24e58b1e95f90b6f9
Elias Jordan <perezjor@aol.com>
2016-05-14 17:58:06 -0500

    Update TextEditor.java

--- TextEditor.java
+++ TextEditor.java
@@ -20,29 +20,25 @@ public class TextEditor extends JFrame
 	// Objects in GUI
 	private JTextArea theText; // Text area to take notes
 	private JPanel textPanel;  // Scrolling text area panel
-	private JMenuBar mBar;     // Horizontal menu bar
-	private JMenu fileMenu;   // Vertical menu for notes
-	private JMenu textMenu;
-	private JMenuItem newFile;
-	private JMenuItem open;
-	private JMenuItem save;
-	private JMenuItem saveAs;
-	private JMenuItem exit;
-	private JRadioButton mono;
-	private JRadioButton serif;
-	private JRadioButton sans;
-	private JCheckBox italic;
-	private JCheckBox bold;
-	private JScrollPane scrollPane;
-	private JFileChooser fileChooser;
-	private ButtonGroup group;
-	private String fileName;
-   
-   
-
-	// THESE ITEMS ARE NOT YET USED
-	// YOU WILL BE CREATING THEM IN THIS LAB
+	private JMenuBar mBar;     // Horizontal menu bar to hold buttons
+	private JMenu fileMenu;   // Vertical menu for save/open etc.
+	private JMenu textMenu;	  //Vertical menu for text aesthetic
+	private JMenuItem newFile; //Button for new file
+	private JMenuItem open;	   //Button to open file chooser to choose existing file	
+	private JMenuItem save;	   //Button save current file
+	private JMenuItem saveAs;  //Button to open file chooser for saving
+	private JMenuItem exit;    //Button to exit the program
+	private JRadioButton mono; //Changes font to monospaced
+	private JRadioButton serif; //Changes font to default serif
+	private JRadioButton sans;  //Changes font to default sans-serif
+	private JCheckBox italic;   //Makes all text italic
+	private JCheckBox bold;     //Makes all text bold
+	private JScrollPane scrollPane;  //In case scrolling is necessary
+	private JFileChooser fileChooser;  //FileChooser for "Save As" and "Open"
+	private ButtonGroup group;     //Group for buttons
+	private String fileName;      //To hold the name of the file
 	private JMenu fontMenu; // Vertical menu for views
+	
 	private JScrollPane scrolledText;   // Scroll bars
 
 	// Default notes
@@ -74,7 +70,7 @@ public class TextEditor extends JFrame
 		mBar = new JMenuBar();
 		mBar.add(fileMenu);
 
-		// ADD THE viewMenu TO THE MENU BAR HERE
+		// Adds the viewMenu to the menu bar
 		mBar.add(textMenu);
 		setJMenuBar(mBar);
 
@@ -87,48 +83,52 @@ public class TextEditor extends JFrame
 		theText.setLineWrap(true);
 		theText.setWrapStyleWord(true);
 
-		// CREATE A JScrollPane OBJECT HERE CALLED
-		// scrolledText AND PASS IN theText, THEN
-		// CHANGE THE LINE BELOW BY PASSING IN scrolledText
+		//Pass the text to the JScrollPane
 		scrollPane = new JScrollPane(theText);
 
-		// textPanel.add(scrolledText);
+		//add the JScrollPane to the ContentPane
 		contentPane.add(scrollPane, BorderLayout.CENTER);
 	}
 
-    /**
+    	/**
 		Method createFile Creates menu to hold 'File' options such as 'New',
 		'Open', 'Save', 'Save As' and 'Exit'
 	*/
 	public void createFile()
 	{
+		//create the JMenu to hold the buttons and set mnemonic
 		fileMenu = new JMenu("File");
 		fileMenu.setMnemonic(KeyEvent.VK_F);
 
+		//creat new button, set mnemonic and add it to the menu
 		newFile = new JMenuItem("New");
 		newFile.setActionCommand("New");
 		newFile.setMnemonic(KeyEvent.VK_N);
 		newFile.addActionListener(new MenuListener());
 		fileMenu.add(newFile);
-
+		
+		//creat open button, set mnemonic and add it to the menu
 		open = new JMenuItem("Open");
 		open.setActionCommand("Open");
 		open.setMnemonic(KeyEvent.VK_O);
 		open.addActionListener(new MenuListener());
 		fileMenu.add(open);
 
+		//creat save button, set mnemonic and add it to the menu
 		save = new JMenuItem("Save");
 		save.setActionCommand("Save");
 		save.setMnemonic(KeyEvent.VK_S);
 		save.addActionListener(new MenuListener());
 		fileMenu.add(save);
 
+		//creat save as button, set mnemonic and add it to the menu
 		saveAs = new JMenuItem("Save As");
 		saveAs.setActionCommand("Save As");
 		saveAs.setMnemonic(KeyEvent.VK_A);
 		saveAs.addActionListener(new MenuListener());
 		fileMenu.add(saveAs);
 
+		//creat exit button, set mnemonic and add it to the menu
 		exit = new JMenuItem("Exit");
 		exit.setActionCommand("Exit");
 		exit.setMnemonic(KeyEvent.VK_X);
@@ -142,32 +142,39 @@ public class TextEditor extends JFrame
 	*/
 	public void createText()
 	{
+		//Create menu to hold font options
 		textMenu = new JMenu("Font");
 		textMenu.setMnemonic(KeyEvent.VK_T);
 
+		//create mono radio button
 		mono = new JRadioButton("Monospaced");
 		mono.setActionCommand("Monospaced");
 		mono.addActionListener(new RadioListener());
 		mono.setSelected(true);
 
+		//create serif radio button
 		serif = new JRadioButton("Serif");
 		serif.setMnemonic(KeyEvent.VK_M);
 		serif.setActionCommand("Serif");
 		serif.addActionListener(new RadioListener());
 
+		//create sans radio button
 		sans = new JRadioButton("SansSerif");
 		sans.setMnemonic(KeyEvent.VK_M);
 		sans.setActionCommand("SansSerif");
 		sans.addActionListener(new RadioListener());
 
+		//create italic check box
 		italic = new JCheckBox("Italic");
 		italic.setActionCommand("Change");
 		italic.addActionListener(new CheckListener());
 
+		//crate bold check box
 		bold = new JCheckBox("Bold");		
 		bold.setActionCommand("Change");
 		bold.addActionListener(new CheckListener());
 
+		//add all of these new buttons to the menu
 		textMenu.add(mono);
 		textMenu.add(serif);
 		textMenu.add(sans);
@@ -175,6 +182,7 @@ public class TextEditor extends JFrame
 		textMenu.add(italic);
 		textMenu.add(bold);
 
+		//group the radio buttons so only one can be selected
 		group = new ButtonGroup();
 		group.add(mono);
 		group.add(serif);
@@ -190,7 +198,7 @@ public class TextEditor extends JFrame
 		public void actionPerformed(ActionEvent e)
 		{
 			String actionCommand = e.getActionCommand();
-			
+
 			if (actionCommand.equals("New"))
 			{
 				fileName = "";
@@ -201,6 +209,7 @@ public class TextEditor extends JFrame
 				fileChooser = new JFileChooser();
 				
 				int result = fileChooser.showOpenDialog(TextEditor.this);
+				
 				if (result == JFileChooser.APPROVE_OPTION)
 				{
 					File file = fileChooser.getSelectedFile();
